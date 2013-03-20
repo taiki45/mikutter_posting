3a2d086342af7ca0394a3bb322d982a3fcac6b44
Taiki ONO <taiks.4559@gmail.com>
2013-03-21 06:06:56 +0900

    add posting and setting field

--- mikutter_posting/AppDelegate.rb
+++ mikutter_posting/AppDelegate.rb
@@ -5,11 +5,33 @@
 #  Created by Taiki on 3/21/13.
 #  Copyright 2013 osakana. All rights reserved.
 #
+require 'socket'
 
 class AppDelegate
   attr_accessor :window
+  attr_accessor :tweet_box
+  attr_accessor :host_box
+  attr_accessor :port_box
+  
   def applicationDidFinishLaunching(a_notification)
-    # Insert code here to initialize your application
+  end
+  
+  def client
+    socket = TCPSocket.new(host_box.stringValue, port_box.intValue)
+    yield socket
+    socket.close
+  end
+  
+  def clear
+    tweet_box.setStringValue("")
+  end
+  
+  def send_text(sender)
+    client {|s| s.write(tweet_box.stringValue) }
+    clear
+  end
+  
+  def clear_text(sender)
+    clear
   end
 end
-
