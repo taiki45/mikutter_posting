0d448f973b3667ed5f1c7045e8530f16ddf03f2f
Taiki ONO <taiks.4559@gmail.com>
2013-03-21 20:09:49 +0900

    add servre function

--- mikutter_posting/AppDelegate.rb
+++ mikutter_posting/AppDelegate.rb
@@ -14,6 +14,7 @@ class AppDelegate
   attr_accessor :port_box
   
   def applicationDidFinishLaunching(a_notification)
+    start_server(server_port)
   end
   
   def client
@@ -33,5 +34,47 @@ class AppDelegate
   
   def clear_text(sender)
     clear
+  end
+
+  def restart_server(sender)
+    stop_server
+    start_server(server_port)
+  end
+
+  private
+
+  def server_port
+    port_box.intValue + 1
+  end
+
+  def start_server(port)
+    @server.kill if @server
+    @server = Server.new(port)
+  end
+
+  def stop_server
+    @server.kill if @server
+    @server = nil
+  end
+
+  class Server
+    def initialize(port)
+      @server = TCPServer.open(port)
+    end
+
+    def run
+      @thread = Thread.new do
+        loop do
+          Thread.start(@server.accept) do |s|
+            lambda {|res| res }.call(s.read)
+            s.close
+          end
+        end
+      end
+    end
+
+    def stop
+      @thread.kill
+    end
   end
 end
